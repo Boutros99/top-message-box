de7080c0fdb539f0b4ffb183df18a5ec0545b8c9
Boutros99 <boutrosestephan@outlook.com>
2024-08-08 15:39:06 +0200

    added the router for message detail

---
+++ routes/messageDetails.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// index controllers 
+
+
+router.get("/details", (req,res) => res.render("message-views/messageDetails.ejs");
+  
+
+module.exports = router
+
